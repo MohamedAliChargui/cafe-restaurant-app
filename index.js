4a4c8406fe0f282ac393c33f7ceb8fa1f584431a
MohamedAliChargui <mohamedalichargui6@gmail.com>
2026-09-10 21:14:29 +0100

    Ajout connexion PostgreSQL avec pg et dotenv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pool = require('./db');
 const app = express();
 const PORT = 3000;
 
@@ -6,6 +7,15 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Bienvenue sur l\'application café-restaurant !');
+});
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.send(`Connexion à la base de données réussie ! Heure serveur : ${result.rows[0].now}`);
+  } catch (err) {
+    res.status(500).send(`Erreur de connexion : ${err.message}`);
+  }
 });
 
 app.listen(PORT, () => {
